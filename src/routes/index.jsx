cbfda5df71b162304537567ef5a76b533f2d0c79
Vikas <sakivksg@gmail.com>
2018-05-25 01:01:08 +0530

    deleted template documentation

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,39 @@
-import Dashboard from "layouts/Dashboard/Dashboard.jsx";
-import Login from "layouts/Login/Login.jsx";
+import React from "react";
+
+function asyncComponent(getComponent) {
+  return class AsyncComponent extends React.Component {
+    static Component = null;
+    state = { Component: AsyncComponent.Component };
+
+    componentWillMount() {
+      if (!this.state.Component) {
+        getComponent().then(Component => {
+          AsyncComponent.Component = Component;
+          this.setState({ Component });
+        });
+      }
+    }
+    render() {
+      const { Component } = this.state;
+      if (Component) {
+        return <Component {...this.props} />;
+      }
+      return null;
+    }
+  };
+}
+
+const Dashboard = asyncComponent(() =>
+  import("layouts/Dashboard/Dashboard.jsx").then(module => module.default)
+);
+const Login = asyncComponent(() =>
+  import("layouts/Login/Login.jsx").then(module => module.default)
+);
 
 const indexRoutes = [
   { path: "/db", component: Dashboard },
-  { path: "/login", component: Login, props:{successURL: "/db"}},
-  { redirect: true, path: "/", to: "/login"}
+  { path: "/login", component: Login, props: { successURL: "/db" } },
+  { redirect: true, path: "/", to: "/login" }
 ];
 
 export default indexRoutes;
